de51b9d810c0c47c088b8ba780174f876423ae86
Colin Halseth <qkmaxware@gmail.com>
2022-08-24 20:58:22 -0700

    Focuser implementation

--- src/AscomAlpaca/Devices/AlpacaTelescope.cs
+++ src/AscomAlpaca/Devices/AlpacaTelescope.cs
@@ -1,12 +1,39 @@
+using Qkmaxware.Measurement;
+
 namespace Qkmaxware.Astro.Control.Devices {
 
 /// <summary>
 /// Interface for controlling telescopes via ASCOM Alpaca
 /// </summary>
 public class AlpacaTelescope : AlpacaDevice, ITelescope {
+        
     internal AlpacaTelescope(AlpacaConnection conn, AlpacaConfiguredDevicesResponse.ConfiguredDevice rawDevice) : base(conn, rawDevice.DeviceName, rawDevice.DeviceType, rawDevice.DeviceNumber) {}
     public AlpacaTelescope(AlpacaConnection conn, string deviceName, string deviceType, int deviceNumber) : base(conn, deviceName, deviceType, deviceNumber) {}
 
+    public Angle RightAscension => throw new System.NotImplementedException();
+
+    public Angle Declination => throw new System.NotImplementedException();
+
+
+    public void Rotate(float horizontal, float vertical)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Sync(Angle ra, Angle dec)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Goto(Angle ra, Angle dec)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Track(Angle ra, Angle dec, TrackingRate rate)
+    {
+        throw new System.NotImplementedException();
+    }
 }
 
 }
