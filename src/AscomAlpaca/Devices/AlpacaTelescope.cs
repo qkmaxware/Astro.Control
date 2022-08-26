80507a17a1874fb72a6b1335f2acb641d510bf8e
Colin Halseth <qkmaxware@gmail.com>
2022-08-25 17:52:05 -0700

    Telescope implementation

--- src/AscomAlpaca/Devices/AlpacaTelescope.cs
+++ src/AscomAlpaca/Devices/AlpacaTelescope.cs
@@ -10,29 +10,78 @@ public class AlpacaTelescope : AlpacaDevice, ITelescope {
     internal AlpacaTelescope(AlpacaConnection conn, AlpacaConfiguredDevicesResponse.ConfiguredDevice rawDevice) : base(conn, rawDevice.DeviceName, rawDevice.DeviceType, rawDevice.DeviceNumber) {}
     public AlpacaTelescope(AlpacaConnection conn, string deviceName, string deviceType, int deviceNumber) : base(conn, deviceName, deviceType, deviceNumber) {}
 
-    public Angle RightAscension => throw new System.NotImplementedException();
+    public Angle RightAscension {
+        get {
+            return Angle.Hours(Get<AlpacaValueResponse<double>>($"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/rightascension").Value);
+        }
+    }
 
-    public Angle Declination => throw new System.NotImplementedException();
+    public Angle Declination {
+        get {
+            return Angle.Degrees(Get<AlpacaValueResponse<double>>($"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/declination").Value);
+        }
+    }
 
 
-    public void Rotate(float horizontal, float vertical)
-    {
+    public void Rotate(float horizontal, float vertical) {
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/abortslew"
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/moveaxis",
+            new KeyValuePair<string,string>("Axis", "0"),
+            new KeyValuePair<string,string>("Rate", horizontal),
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/moveaxis",
+            new KeyValuePair<string,string>("Axis", "1"),
+            new KeyValuePair<string,string>("Rate", vertical),
+        );
         throw new System.NotImplementedException();
     }
 
-    public void Sync(Angle ra, Angle dec)
-    {
-        throw new System.NotImplementedException();
+    public void Sync(Angle ra, Angle dec) {
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/synctocoordinates",
+            new KeyValuePair<string,string>("RightAscension", ((double)ra.TotalHours()).ToString()),
+            new KeyValuePair<string,string>("Declination", ((double)dec.TotalDegrees()).ToString()),
+        );
     }
 
-    public void Goto(Angle ra, Angle dec)
-    {
-        throw new System.NotImplementedException();
+    public void Goto(Angle ra, Angle dec) {
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/targetrightascension",
+            new KeyValuePair<string,string>("TargetRightAscension", ((double)ra.TotalHours()).ToString())
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/targetdeclination",
+            new KeyValuePair<string,string>("TargetDeclination", ((double)dec.TotalDegrees()).ToString())
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/tracking",
+            new KeyValuePair<string,string>("Tracking", false.ToString())
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/slewtotargetasync"
+        );
     }
 
-    public void Track(Angle ra, Angle dec, TrackingRate rate)
-    {
-        throw new System.NotImplementedException();
+    public void Track(Angle ra, Angle dec, TrackingRate rate) {
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/targetrightascension",
+            new KeyValuePair<string,string>("TargetRightAscension", ((double)ra.TotalHours()).ToString())
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/targetdeclination",
+            new KeyValuePair<string,string>("TargetDeclination", ((double)dec.TotalDegrees()).ToString())
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/slewtotargetasync"
+        );
+        Put<AlpacaMethodResponse>(
+            $"{Connection.Server.Host}:{Connection.Server.Port}/telescope/{DeviceNumber}/tracking",
+            new KeyValuePair<string,string>("Tracking", true.ToString())
+        );
     }
 }
 
